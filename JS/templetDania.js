a9325d502b365f50a7d4df08dc8e33fd749277b1
DaniaAbughoush <ms1dodo@gmail.com>
2021-04-06 10:54:25 +0300

    adding pdf

--- JS/templetDania.js
+++ JS/templetDania.js
@@ -5,7 +5,7 @@ window.onload = function () {
       console.log(downloadPdf);
       console.log(window);
       let opt = {
-        margin:.2,
+        margin:.05,
         filename: 'myfile.pdf',
         image: { type: 'jpeg', quality: 0.98 },
         html2canvas: { scale: 2 },
@@ -15,4 +15,61 @@ window.onload = function () {
     });
 };
 
+// console.log(localStorage.getItem('Experiance'));
+// console.log(localStorage.getItem('Education'));
+// console.log(localStorage.getItem('Experiance'));
+// console.log(localStorage.getItem('personality'));
+// console.log(localStorage.getItem('Skills'));
+// console.log(localStorage.getItem('Language'));
 
+
+
+let education=JSON.parse(localStorage.getItem('Education'));
+let experiance=JSON.parse(localStorage.getItem('Experiance'));
+let personality=JSON.parse(localStorage.getItem('personality'));
+let skills=JSON.parse(localStorage.getItem('Skills'));
+let languges=JSON.parse(localStorage.getItem('Language'));
+
+ let usern=document.getElementById('username');
+ usern.innerHTML=personality.firstName+ ' '+ personality.lastName;
+
+ let phone=document.getElementById('phone');
+phone.innerHTML='Phone Number:'+personality.telephonePersonal;
+
+let email=document.getElementById('email');
+email.innerHTML='Email:'+personality.eMail;
+let addres=document.getElementById('add1');
+addres.innerHTML='Adress:'+personality.locAddress;
+
+
+
+let edu=document.getElementById('edu');
+const liEl=document.createElement('li');
+edu.appendChild(liEl);
+
+liEl.innerHTML=education.unName+' '+education.unMajor+' '+education.yearOfGraduate+' '+education.unDegree;
+ 
+let exp=document.getElementById('exp');
+let liEl2=document.createElement('li');
+exp.appendChild(liEl2);
+liEl2.innerHTML=experiance.tiJob+' '+experiance.orgName+' ' +experiance.starAt+' '+experiance.finishAt;
+
+
+let skil=document.getElementById('skill');
+let liEl3=document.createElement('li');
+skil.appendChild(liEl3);
+liEl3.innerHTML=skills.skillName+' '+skills.placeOfLearning+' '+skills.starAt+' '+skills.finishAt;
+
+
+let lan=document.getElementById('lan');
+let liEL4=document.createElement('li');
+lan.appendChild(liEL4);
+liEL4.innerHTML=languges.langSelected+' '+languges.rateLanguage;
+
+
+
+
+
+// console.log(experiance.orgName);
+// let x=document.getElementById('username');
+// x.textContent=experiance.orgName;
